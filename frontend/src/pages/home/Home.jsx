21717b4a592d077c4788f95aafa92deb7d131539
Ramya-rem <162437159+Ramya-rem@users.noreply.github.com>
2024-07-06 07:28:31 +0530

    commit with responsive design

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import MessageContainer from '../../components/messages/MessageContainer';
 import Sidebar from '../../components/sidebar/Sidebar'
 
 const Home = () => {
-  return <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 mt-20'>
+  return <div className='flex h-[300px] sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 mt-20'>
       {/* sidebar and megcontainer will be flex so sidebar in left side and msgcontainer is on left side  */}
       <Sidebar />
       <MessageContainer />
