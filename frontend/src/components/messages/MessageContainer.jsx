ee0e627da43639b34931576e9844364357b310db
Ramya-rem <162437159+Ramya-rem@users.noreply.github.com>
2024-07-09 14:20:29 +0530

    commit with responsive design

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -17,7 +17,7 @@ const MessageContainer = () => {
         return ()=> setSelectedConversation(null);//default; again opn panna select panna chat kattadhu so using null
     },[setSelectedConversation]);
     return (
-        <div className="flex flex-col w-full h-full sm:h-auto">
+        <div className="md:min-w-[450px] flex flex-col w-full sm:w-auto h-full sm:h-auto">
             {!selectedConversation ? ( <NoChatSelected /> ) :(
                 //if doesn't select any chat nochatselected will be shown else below component will be shown
                 <>
@@ -30,12 +30,8 @@ const MessageContainer = () => {
                     </button>
                 </div>
                 {/* below the header we have messages */}
-                <div className="flex-grow overflow-y-auto">
                 <Messages />
-                </div>
-                <div className="flex-shrink-0">
               <MessageInput /> 
-              </div>
             </>
             )}
         </div>
