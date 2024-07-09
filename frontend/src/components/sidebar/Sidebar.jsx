df02567771e37cc94935ad737cef1f2d7114d0db
Ramya-rem <162437159+Ramya-rem@users.noreply.github.com>
2024-07-09 16:24:24 +0530

    commit with responsive design

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -5,10 +5,12 @@ import SearchInput from "./SearchInput"
 
 const Sidebar = () => {
   return (
-    <div  className='border-r border-slate-500 p-4 flex flex-col h-full sm:h-auto overflow-y-auto'>
+    <div  className='border-r border-slate-500 p-4 flex flex-col h-full sm:h-auto'>
       <SearchInput />
       <div className="divider px-3"></div>
+      <div className="flex-grow overflow-y-auto">
     <Conversations />
+    </div>
     <LogoutButton />
     </div>
 
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
   <div className='flex flex-col sm:flex-row sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 mt-20'>
       {/* sidebar and megcontainer will be flex so sidebar in left side and msgcontainer is on left side  */}
-    <div className={`sm:flex ${selectedConversation ? 'hidden' : 'flex'} w-full sm:w-auto`}>
+    <div className={`sm:flex ${selectedConversation ? 'hidden' : 'flex'} w-full sm:w-auto sm:flex-shrink-0`}>
       <Sidebar />
       </div>
       <div className={`flex-grow ${selectedConversation ? 'flex' : 'hidden sm:flex'}`}>
