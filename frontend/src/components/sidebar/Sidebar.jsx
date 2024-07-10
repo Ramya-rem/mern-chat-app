5c8f53a338cadb19243a505c9d93c04986e4e338
Ramya-rem <162437159+Ramya-rem@users.noreply.github.com>
2024-07-10 19:44:34 +0530

    commit with responsive design

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -4,10 +4,10 @@ import SearchInput from "./SearchInput"
 
 const Sidebar = () => {
   return (
-    <div  className='border-r border-slate-500 p-4 flex flex-col h-full sm:h-auto'>
+    <div  className='border-r border-slate-500 p-4 flex flex-col h-full sm:h-auto overflow-y-auto'>
       <SearchInput />
       <div className="divider px-3"></div>
-      <div className="flex-grow overflow-y-auto">
+      <div className="flex-grow overflow-auto">
     <Conversations />
     </div>
     <LogoutButton />
