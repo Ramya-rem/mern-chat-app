ade86ebbab77cb51a51471af09f7d167b954c509
Ramya-rem <162437159+Ramya-rem@users.noreply.github.com>
2024-07-10 08:55:35 +0530

    commit with responsive design

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,20 +1,15 @@
-import React from 'react';
 import Conversations from "./Conversations"
 import LogoutButton from "./LogoutButton"
 import SearchInput from "./SearchInput"
 
 
-const Sidebar = ({ isVisible, onConversationSelect }) => {
-  const handleConversationClick = (conversation) => {
-    onConversationSelect(conversation); // Call the callback prop with selected conversation
-  };
+const Sidebar = () => {
+  
   return (
-    <div  className={`border-r border-slate-500 p-4 flex flex-col ${
-      isVisible ? '' : 'hidden sm:block' // Show sidebar on mobile only when conversation is selected
-    }`}>
+    <div  className='border-r border-slate-500 p-4 flex flex-col'>
       <SearchInput />
       <div className="divider px-3"></div>
-    <Conversations onClick={handleConversationClick} /> {/* Pass onClick handler to Conversations */}
+    <Conversations />
     <LogoutButton />
     </div>
 
