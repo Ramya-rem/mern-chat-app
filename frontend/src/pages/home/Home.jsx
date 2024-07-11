b4f6b8089fdb21e5bb02fb9962da013abeb07956
Ramya-rem <162437159+Ramya-rem@users.noreply.github.com>
2024-07-11 12:11:31 +0530

    commit with responsive design

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -6,9 +6,9 @@ import useConversation from '../../zustand/useConversation';
 const Home = () => {
   const { selectedConversation } = useConversation();
   return (
-  <div className='flex flex-col sm:flex-row rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 lg:mt-20 sm:mt-0 h-screen' >
+  <div className='flex flex-col sm:flex-row sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 lg:mt-20 sm:mt-0 h-full' >
       {/* sidebar and megcontainer will be flex so sidebar in left side and msgcontainer is on left side  */}
-      <div className={`flex ${selectedConversation ? 'hidden' : 'flex'} w-full sm:w-auto sm:flex-grow-0`}>
+      <div className={`sm:flex ${selectedConversation ? 'hidden' : 'flex'} w-full sm:w-auto`}>
         <Sidebar />
         </div>
         <div className={`flex-grow ${selectedConversation ? 'flex' : 'hidden sm:flex'} h-full`}>
